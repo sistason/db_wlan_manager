1315695b68dde5717c867b5c0665ef9fa8a5ddd5
Kai Sisterhenn <sistason@sistason.de>
2017-08-04 13:49:07 +0200

    fixed without API again, as the url+"/de/" was universally added. Still only international ICE seem to have the omboard-API? Simplified interface.py, as we only need it to randomize_mac, which comes after quota, which is known after API-call, which sends the right MAC :)

--- interface.py
+++ interface.py
@@ -1,22 +1,47 @@
 import re
 import logging
 import subprocess
+import random
 
 
 class Interface:
 
-    def __init__(self):
-        self.name = self._get_correct_interface()
-        if self.name is None:
+    def __init__(self, mac):
+        self.original_mac_address = mac
+        self.interface_name = self._get_correct_interface(mac)
+        print(self.interface_name)
+        if self.interface_name is None:
             logging.info('Wifi-connection most likely broken')
-        self.mac_address = self._get_mac_from_interface(self.name)
-        if not self.mac_address:
-            logging.warning('Could not get MAC address')
 
-    def _get_mac_from_interface(self, interface):
+    def close(self):
+        """ Leave the interface with the MAC it had when this script started """
+        if self._get_mac() != self.original_mac_address:
+            self._set_mac(self.original_mac_address)
+
+    def randomize_mac(self):
+        """ Randomize a MAC-address and set it """
+        new_mac = ':'.join([hex(random.randrange(16*16)) for _ in range(6)])
+        self._set_mac(new_mac)
+
+    def _set_mac(self, mac):
+        """ Set the supplied MAC-address to our interface """
+        int_down_call = subprocess.run(['ip', 'link', 'set', self.interface_name, 'down'], stderr=subprocess.PIPE)
+        if int_down_call.returncode:
+            logging.error('Could not get the interface down: {}'.format(int_down_call.stderr))
+            return
+        mac_call = subprocess.run(['ip', 'link', 'set', self.interface_name, 'address', mac], stderr=subprocess.PIPE)
+        int_up_call = subprocess.run(['ip', 'link', 'set', self.interface_name, 'up'], stderr=subprocess.PIPE)
+        if mac_call.returncode:
+            logging.error('Could not change MAC-address: {}'.format(mac_call.stderr))
+        elif int_up_call.returncode:
+            logging.error('Could not get the interface back up: {}'.format(int_up_call.stderr))
+        else:
+            return True
+
+    def _get_mac(self):
         """ Get MAC-address by calling ip link show and parsing that output """
-        if interface:
-            mac_string = self._get_mac_string_from_interface(interface)
+        if self.interface_name:
+            mac_string = self._get_mac_string_from_interface(self.interface_name)
             return self._get_mac_from_device(mac_string)
 
     @staticmethod
@@ -35,75 +60,19 @@ class Interface:
         match_ = re.search(r'\s([0-9a-f]{2}:){5}([0-9a-f]{2})\s', mac_string)
         return match_.group(0).strip() if match_ else None
 
-    def _get_correct_interface(self):
-        """ Get the interface on which to connect to wifionice
-        Check routing table for
-        1. static routes to 172.16.0.1,
-        2. default routes via 172.16.0.1
-        to get the interface by the lowest metric
-        """
-        static_routes = self._get_static_routes()
-        default_routes = self._get_default_routes()
-        route_by_subnet = self._get_route_by_subnet()
-
-        if static_routes:
-            best_static_route = min(static_routes, key=lambda f: f[0])
-            return self._get_interface_from_route(best_static_route[1])
-
-        if default_routes:
-            best_default_route = min(default_routes, key=lambda f: f[0])
-            return self._get_interface_from_route(best_default_route[1])
-
-        if route_by_subnet:
-            best_route_by_subnet = min(route_by_subnet, key=lambda f: f[0])
-            return self._get_interface_from_route(best_route_by_subnet[1])
-
-        return None
-
-    def _get_static_routes(self):
-        """ Get all static routes to 172.16.0.1 """
-        return self._filter_all_routes_by_re(re.compile(r'^172.16.0.1(/|\s)'))
-
-    def _get_default_routes(self):
-        """ Get all default routes via 172.16.0.1 """
-        return self._filter_all_routes_by_re(re.compile(r'default\s+via\s172.16.0.1'))
-
-    def _get_route_by_subnet(self):
-        """ Get the specific interface route by the 172.16.0.0/16 subnet,
-            At this point, there is no way to check if we're online, since we have no connection to wifionice
-        """
-        return self._filter_all_routes_by_re(re.compile(r'^172.16.0.0/16\s'))
-
-    def _filter_all_routes_by_re(self, pattern):
-        """
-        :param pattern: Regular Expression pattern to filter the routes
-        :return: A list of routes matching that pattern
-        """
-        all_routes = self._get_all_routes()
-        return [(self._get_metric_from_route(route), route) for route in all_routes
-                if pattern.search(route)]
+    def _get_correct_interface(self, mac):
+        """ Get the interface on which to connect to wifionice """
+        interfaces = self._get_all_interfaces()
+        for interface in interfaces:
+            match = re.search(r'(?s)^(?P<interface>[^:]+):.*link/ether (?P<mac>[\dabcdef:]+) ', interface)
+            if match:
+                iface_, mac_ = match.group('iface'), match.group('mac')
+                if mac_ == mac:
+                    return iface_
 
     @staticmethod
-    def _get_all_routes():
-        """ Wrap call to ip ro and return a list of route-strings """
-        all_routes = subprocess.run(['ip', 'ro'], stdout=subprocess.PIPE)
-        if all_routes.returncode == 0:
-            return all_routes.stdout.decode().split('\n')
-
-    @staticmethod
-    def _get_metric_from_route(route):
-        """
-        :param route: A route string from ip2
-        :return: The route metric or 0, if there is none found
-        """
-        match_ = re.search(r'\smetric\s(\d+)\s', route)
-        return int(match_.group(1)) if match_ else 0
-
-    @staticmethod
-    def _get_interface_from_route(route):
-        """
-        :param route: A route string from ip2
-        :return: The device name or None, if there is none found
-        """
-        match_ = re.search(r'\sdev\s(\w+)\s', route)
-        return match_.group(1) if match_ else None
+    def _get_all_interfaces():
+        """ Wrap call to ip link and return a list of interface-strings """
+        all_interfaces = subprocess.run(['ip', 'link', 'show'], stdout=subprocess.PIPE)
+        if all_interfaces.returncode == 0:
+            return re.split(r'(?m)[\n]\d+: ', '\n'+all_interfaces.stdout.decode())[1:]
--- manager.py
+++ manager.py
@@ -4,6 +4,8 @@ import time
 import requests
 import logging
 import dns.resolver
+import json
+import re
 
 from interface import Interface
 
@@ -12,51 +14,130 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 
 class DBManager:
     url = "www.wifionice.de"
+    api_url = "https://www.ombord.info/api/jsonp/user/"
     url_cached_ip = None
+    new_api = None
 
     def __init__(self, user_mode):
         self.user_mode = user_mode
         self.is_online = None
         self.interface = None
+        self.json_decoder = json.JSONDecoder()
+        self.quota = (0, 0)
 
         self.resolver = dns.resolver.Resolver()
         self.resolver.nameservers = ['172.16.0.1']
         self.url_cached_ip = self.resolve_url()
 
-        if not user_mode:
-            self.interface = Interface()
-
     def run(self):
-        while not time.sleep(1):
-            self.update_online()
-            if self.is_online:
-                print('.', end='', flush=True)
-                continue
-            self.login()
+        try:
+            while not time.sleep(1):
+                self.update_online()
+                if self.is_online:
+                    self.manage_bandwidth()
+                    print('.', end='', flush=True)
+                    continue
+                self.login()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            if self.interface is not None:
+                self.interface.close()
+
+    def manage_bandwidth(self):
+        if self.quota == (0, 0):
+            # Without quota (old website), this does not work
+            return
+
+        if self.quota[0] >= self.quota[1]:
+            if self.user_mode:
+                logging.info('Your traffic is being shaped, as you surpassed your data limit')
+                logging.info('To automatically reset your data limit, run this script as root')
+                return
+
+            self.interface.randomize_mac()
+
+    def _check_api(self):
+        try:
+            ret = requests.get(self.api_url, timeout=5)
+            if ret.status_code == 404:
+                self.new_api = False
+                logging.debug('Using old API...')
+            else:
+                self.new_api = True
+                logging.debug('Using new API...')
+        except requests.ConnectionError as e:
+            logging.debug('Connection Error: {}'.format(e))
 
     def update_online(self):
+        if self.new_api is None:
+            if self._check_api() is None:
+                return
+
+        on = self.update_online_with_api() if self.new_api else self.update_online_without_api()
+        if on is False:
+            if self.is_online is True or self.is_online is None:
+                logging.info('I am not online anymore! :(')
+            self.is_online = False
+        elif on is True:
+            if self.is_online is False or self.is_online is None:
+                logging.info('I am online again! :)')
+            self.is_online = True
+
+    def update_online_without_api(self):
         """ Check if we are online. Don't change the state if the check fails itself """
-        ret = self._do_request()
+        ret = requests.get('http://' + self.url_cached_ip + '/de/')
         if ret and ret.status_code == 200:
             txt = ret.text.lower()
             if txt.count('offline') > 5:
-                if self.is_online is True or self.is_online is None:
-                    logging.info('I am not online anymore! :(')
-                self.is_online = False
+                return False
             if txt.count('online') > 5:
-                if self.is_online is False or self.is_online is None:
-                    logging.info('I am online again! :)')
-                self.is_online = True
-
+                return True
         else:
             print('?', end=' ', flush=True)
             logging.debug('Return object from wifionice broken!: {}'.format(ret))
+
+    def update_online_with_api(self):
+        """
+        "version":"1.9", "ip":"172.16.100.116", "mac":"6C:88:14:84:84:88", "online":"0", "timeleft":"0",
+        "authenticated":"1", "userclass":"2", "expires":"Never", "timeused":"1206", "data_download_used":"9256202",
+        "data_upload_used":"4302103", "data_total_used":"13558305", "data_download_limit":"209715200",
+        "data_upload_limit":"0", "data_total_limit":"0", "bandwidth_download_limit":"81250",
+        "bandwidth_upload_limit":"81250", "cap_level":"0"
+        """
+        status = self._get_status_from_api()
+        print(status)
+        if self.interface is None and len(status.get('mac', '')) == 17:
+            # Interface uninitialized
+            self.interface = Interface(status.get('mac'))
+
+        self.update_quota(status)
+
+        return status.get('online') == "1"
+
+    def update_quota(self, user):
+        quota = user.get('data_download_used', '')
+        limit = user.get('data_download_limit', '')
+
+        if quota.isdigit() and limit.isdigit():
+            self.quota = (quota, limit)
+
+    def _get_status_from_api(self):
+        try:
+            ret = requests.get(self.api_url, timeout=5)
+            return self.json_decoder.decode(re.sub(r'[\n\(\); ]', '', ret.text)[:-2] + '}')
+        except requests.ConnectTimeout:
+            return {}
+        except Exception as e:
+            logging.error('Error getting API response, was {}'.format(ret.text))
+            return {}
 
     def login(self):
         """ Log in to the ICE Portal (wifionice) """
         logging.info('Trying to log in...')
         data = {}
-        self._do_request(url_suffix='?login', post=True, data=data)
+        suffix = '/de/?login'
+        self._do_request(url_suffix=suffix, post=True, data=data)
 
     def _do_request(self, url_suffix='', post=False, data=None):
         """ Wrapper around the requests.get/post, for timeout/error handling"""
@@ -64,7 +145,6 @@ class DBManager:
             data = {}
 
         address = self.url_cached_ip if self.url_cached_ip else self.url
-        url_suffix = '/de/' + url_suffix
 
         retries = 3
         while retries > 0:
@@ -94,9 +174,7 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     logging.basicConfig(format='%(message)s',
-                        level=logging.INFO)
-    # import pdb
-    # pdb.set_trace()
+                        level=logging.DEBUG)
 
     manager = DBManager(args.user_mode)
     try:
